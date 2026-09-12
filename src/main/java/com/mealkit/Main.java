03299a2952626a7f2f2f22ac9d79ba137a1fae6b
DamirKus0 <justkusta@gmail.com>
2026-09-13 00:06:14 +0500

    Add some tests in main class

--- src/main/java/com/mealkit/Main.java
+++ src/main/java/com/mealkit/Main.java
@@ -1,8 +1,19 @@
 package com.mealkit;
 import com.mealkit.builder.RecipeDirector;
+import com.mealkit.model.Recipe;
 public class Main {
     public static void main(String[] args) {
-        RecipeDirector director = new RecipeDirector();
-
+        Recipe myBurger = new Recipe.Builder("Burger", "Fast Food", 1, 15)
+                .withSauce("BBQ")
+                .withSpiciness(1)
+                .withNutrition(600, 25, 30, 50)
+                .build();
+        System.out.println(myBurger);
+        Recipe Sushi = new Recipe.Builder("Sushi", "Fast Food", 8, 20)
+                .withSauce("Soeviy sous")
+                .withSpiciness(0)
+                .withNutrition(500,25,20,40)
+                .build();
+        System.out.println(Sushi);
     }
 }
